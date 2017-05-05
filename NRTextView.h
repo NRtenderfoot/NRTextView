ed9e5a9a93b040f007ffdbbac64d7ff2053c9968
neetika <neetika@xicoms-iMac-3.local>
2017-05-05 18:11:23 +0530

    First Commit

---
+++ NRTextView.h
@@ -0,0 +1,33 @@
+//
+//  NRTextView.h
+//  Fraymo
+//
+//  Created by neetika  on 1/23/17.
+//  Copyright © 2017 Neetika Rana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef void(^callTagPressed)(NSString* tag);
+
+IB_DESIGNABLE
+
+@interface NRTextView : UITextView
+
+@property (nonatomic, retain) IBInspectable UIColor *borderClolor;
+@property (assign, nonatomic) IBInspectable CGFloat borderWidth;
+@property (assign, nonatomic) IBInspectable CGFloat cornerRadius;
+@property (nonatomic, retain) IBInspectable UIColor *hashTagColor;
+@property (nonatomic, retain) IBInspectable UIColor *atSignColor;
+@property (nonatomic, retain) IBInspectable UIColor *preferredTextColor;
+
+@property (nonatomic, assign) IBInspectable BOOL enableHashTag;
+@property (nonatomic, assign) IBInspectable BOOL enableAtSign;
+
+@property (nonatomic, assign) UIFont *specialFont;
+
+@property (copy, nonatomic) callTagPressed callBackAction;
+
+- (void)setText:(NSString *)text withImage:(UIImage *)image;
+
+@end
